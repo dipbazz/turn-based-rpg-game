dd05c8cacfa54ee41283dbe149b364630aee35ba
Dipesh Bajgain <dipbazz@gmail.com>
2021-04-05 18:10:39 +0545

    Update the damage for dragons

--- src/Scenes/BattleScene.js
+++ src/Scenes/BattleScene.js
@@ -95,10 +95,10 @@ export default class BattleScene extends Phaser.Scene {
     let mage = new Player(this, 250, 100, 'player', 4, 'Mage', 80, 8);
     this.add.existing(mage);
 
-    let dragonblue = new Enemy(this, 50, 50, 'blueDragon', null, 'Dragon', 50, 3);
+    let dragonblue = new Enemy(this, 50, 50, 'blueDragon', null, 'Dragon', 50, 15);
     this.add.existing(dragonblue);
 
-    let dragonorange = new Enemy(this, 50, 100, 'orangeDragon', null, 'Dragon2', 50, 3);
+    let dragonorange = new Enemy(this, 50, 100, 'orangeDragon', null, 'Dragon2', 50, 10);
     this.add.existing(dragonorange);
 
     this.heroes = [warrior, mage];
